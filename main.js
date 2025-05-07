3ee313bc4793f675dd6adbf7d4b461b4db0919a6
luanalesnioski <145451858+luanalesnioski@users.noreply.github.com>
2025-05-07 11:12:26 -0300

    1

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-01-10T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-20T00:00:00");
-const tempoObjetivo3 = new Date("2025-7-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-11-05T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
@@ -47,10 +47,7 @@ function atualizaCronometro() {
         document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
         document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
-      }
-  for (let i = 0; i < contadores.length; i++) { 
-    document.getElementById ("seg"+i). textContent= calculaTempo(tempos[i])[3];
-    consolo.log("seg"+i);
+    }
 }
 
 function comecaCronometro() {
